91a9d3a07a0742cf762c293d59812bab89a2aa46
linhbhvu1999 <91562039+linhbhvu1999@users.noreply.github.com>
2021-12-14 16:11:53 -0600

    final

--- config/config.js
+++ config/config.js
@@ -6,20 +6,20 @@ var config = {
        rootPath: path.normalize(__dirname + '/..'),
        app: { name: ' CRMLite' },
        port: 80,
-       dbURL: 'mongodb+srv://linhvu:<password>@cluster0.1qdjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+       dbURL: 'mongodb+srv://linhvu:Connhonhaqua96,@cluster0.1qdjj.mongodb.net/test',
        secret: "cayennedlikedhistreats"
 },
        test: {
               rootPath: path.normalize(__dirname + '/..'),
               app: { name: ' CRMLite' },
               port: 80,
-              dbURL: "mongodb+srv://linhvu:<password>@cluster0.1qdjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+              dbURL: "mongodb+srv://linhvu:Connhonhaqua96,@cluster0.1qdjj.mongodb.net/test"
        },
        production: {
               rootPath: path.normalize(__dirname + '/..'),
               app: { name: ' CRMLite' },
               port: 8080,
-              dbURL: 'mongodb+srv://linhvu:<password>@cluster0.1qdjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+              dbURL: 'mongodb+srv://linhvu:Connhonhaqua96,@cluster0.1qdjj.mongodb.net/test',
               secret: "cayennedlikedhistreats"
        } 
 };
